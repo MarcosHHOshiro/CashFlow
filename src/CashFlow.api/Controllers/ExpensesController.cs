08695e6f4266544307b38cf0f91a556cbb0f94cd
Marcos <marcos.2h.oshiro@gmail.com>
2025-12-04 23:11:59 -0400

    primeiro commit

---
+++ src/CashFlow.api/Controllers/ExpensesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CashFlow.Communication.Requests;
+using CashFlow.Application.UseCase.Expenses.Register;
+
+namespace CashFlow.api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+        var useCase = new RegisterExpenseUseCase();
+
+        var response = useCase.Execute(request);
+
+        return Created(string.Empty, response);
+    }
+
+}
