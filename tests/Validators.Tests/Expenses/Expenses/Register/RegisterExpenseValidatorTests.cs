7ae9c65b97343e06c9bf1bdb493d1d2d89ee41ab
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-12-12 16:20:09 -0400

    first success test

---
+++ tests/Validators.Tests/Expenses/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using CashFlow.Application.UseCase.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Expenses.Register;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            Description = "description",
+            Title = "Apple",
+            PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard
+        };
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid);
+    }
+}
+
