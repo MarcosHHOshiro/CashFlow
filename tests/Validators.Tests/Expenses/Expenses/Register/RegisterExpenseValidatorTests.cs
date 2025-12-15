641d1ef0e18fb5cd4fa0536529e28f90661c8d5e
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-12-15 13:27:45 -0400

    Finishin validations tests

--- tests/Validators.Tests/Expenses/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCase.Expenses.Register;
+using CashFlow.Communication.Enums;
 using CashFlow.Excetiom;
 using CommonTestUtilities.Requests;
 using Shouldly;
@@ -56,5 +57,46 @@ public class RegisterExpenseValidatorTests
             errors => errors.Single().ErrorMessage.ShouldBe(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE)
         );
     }
+
+    [Fact]
+    public void Error_Payment_Type_Invalid()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.PaymentType = (PaymentType)700;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(
+            errors => errors.Single().ErrorMessage.ShouldBe(ResourceErrorMessages.PAYMENT_TYPE_INVALID)
+        );
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-2)]
+    [InlineData(-7)]
+    public void Error_Amount_Invalid(decimal amount)
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Amount = amount;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(
+            errors => errors.Single().ErrorMessage.ShouldBe(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO)
+        );
+    }
+
 }
 
