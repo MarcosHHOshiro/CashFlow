6169924b8d335897c233abff065647de15ce9bf3
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-12-15 12:31:47 -0400

    Creating error tests

--- tests/Validators.Tests/Expenses/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCase.Expenses.Register;
+using CashFlow.Excetiom;
 using CommonTestUtilities.Requests;
 using Shouldly;
 
@@ -20,8 +21,40 @@ public class RegisterExpenseValidatorTests
         result.IsValid.ShouldBeTrue();
     }
 
-    public void Failure() { 
-    
+    [Fact]
+    public void Error_Title_Empty()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(
+            errors => errors.Single().ErrorMessage.ShouldBe(ResourceErrorMessages.TITLE_REQUIRED)
+        );
+    }
+
+    [Fact]
+    public void Error_Date_Future()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(1);
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(
+            errors => errors.Single().ErrorMessage.ShouldBe(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE)
+        );
     }
 }
 
