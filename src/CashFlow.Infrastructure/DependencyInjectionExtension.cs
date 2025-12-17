700b71d7f615f64d79ed3af8bc0eb01809797fa7
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-12-17 15:51:52 -0400

    using DI com DbContext

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +9,16 @@ public static class DependencyInjectionExtension
 {
     public static void AddInfrastructure(this IServiceCollection services)
     {
+        AddRepositories(services);
+        AddDbContext(services);
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
         services.AddScoped<IExpenseRepository, ExpensesRepository>();
+    }
+    private static void AddDbContext(IServiceCollection services)
+    {
+        services.AddDbContext<CashFlowDbContext>();
     }
 }
