7e91325a04e03fcc55dd4abe747bc13c628e4fc0
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-12-18 14:09:48 -0400

    Automapper

--- src/CashFlow.Application/UseCase/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCase/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using AutoMapper;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories;
@@ -11,30 +12,28 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
 {
     private readonly IExpenseRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
-    public RegisterExpenseUseCase(IExpenseRepository repository, IUnitOfWork unitOfWork)
+    private readonly IMapper _mapper;
+    public RegisterExpenseUseCase(
+        IExpenseRepository repository,
+        IUnitOfWork unitOfWork,
+        IMapper mapper)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
 
     public async Task<ResponseRegisterExpenseJson> Execute(RequestRegisterExpenseJson request)
     {
         Validate(request);
 
-        var entity = new Expense
-        {
-            Amount = request.Amount,
-            Date = request.Date,
-            Description = request.Description,
-            Title = request.Title,
-            PaymentType = (Domain.Enuns.PaymentType)request.PaymentType
-        };
+        var entity = _mapper.Map<Expense>(request);
 
         await _repository.Add(entity);
 
         await _unitOfWork.Commit();
 
-        return new ResponseRegisterExpenseJson();
+        return _mapper.Map<ResponseRegisterExpenseJson>(entity);
     }
 
     private void Validate(RequestRegisterExpenseJson request)
@@ -46,7 +45,7 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
         if (result.IsValid == false)
         {
             var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
-            
+
             throw new ErrorOnValidationException(errorMessages);
         }
     }
